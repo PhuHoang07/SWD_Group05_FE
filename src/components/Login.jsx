84bea94464a83100ccbf3d23950667213bc50c92
Nguyen Vi Tien <Nguyevitien2003@gmail.com>
2024-06-06 12:47:43 +0700

    [Feat] Fetch API

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,66 +1,161 @@
-import React, { useState } from 'react'
-import './Login.css'
-import { FaUser , FaLock, FaEnvelope} from "react-icons/fa";
-
+import React, { useState } from 'react';
+import './Login.css';
+import { FaUser, FaLock, FaEnvelope, FaPhone } from "react-icons/fa";
 
 const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [fullname, setFullname] = useState('');
+  const [isLoginActive, setIsLoginActive] = useState(true);
+  const [errorMessage, setErrorMessage] = useState('');
 
-    const [action, setAction] = useState('');
+  const toggleActive = () => {
+    setIsLoginActive(!isLoginActive);
+    setErrorMessage('');
+  };
 
-    const [isLoginActive, setIsLoginActive] = useState(true);
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('https://localhost:7285/api/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email: username,
+          password: password
+        }),
+        mode: 'cors' 
+      });
 
-    const toggleActive = () => {
-        setIsLoginActive(!isLoginActive);
-    };
-    const wrapperClass = isLoginActive ? 'wrapper' : 'wrapper active';
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+      } else {
+        const errorData = await response.json();
+        setErrorMessage(errorData.message);
+        console.error('Login failed:', errorData.message);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('https://localhost:7285/api/auth/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          fullname: fullname,
+          email: email,
+          phoneNumber: phoneNumber
+        }),
+        mode: 'cors'
+      });
+
+      if (response.ok) {
+        setIsLoginActive(true);
+        setErrorMessage('');
+        const data = await response.json();
+        console.log(data);
+      } else {
+        const errorData = await response.json();
+        setErrorMessage(errorData.message);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  const wrapperClass = isLoginActive ? 'wrapper' : 'wrapper active';
 
   return (
     <div className={wrapperClass}>
       <div className='form-box login'>
-        <form action=''>
-            <h1>Login</h1>
-            <div className='input-box'>
-                <input type='text' placeholder='Username' required />    
-                <FaUser className='icon' /> 
-            </div>
-            <div className='input-box'>
-            <input type='password' placeholder='Password' required />
-                <FaLock className='icon' />
-            </div>
-            <div className='remember-forgot'>
-                <label><input type='checkbox' />Remember me</label>
-                <a href='#'>Forgot Password?</a>
-            </div>
-            <button type='submit' >Login</button>
-            <div className='register-link'>
-                <p>Don't have an account? <a href='#' onClick={toggleActive}>Register</a></p>
-            </div>
+        <form onSubmit={handleLogin}>
+          <h1>Login</h1>
+          <div className='input-box'>
+            <FaUser className='icon' /> 
+            <input 
+              type='text' 
+              placeholder='Username' 
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required 
+            />    
+          </div>
+          <div className='input-box'>
+            <FaLock className='icon' />
+            <input 
+              type='password' 
+              placeholder='Password' 
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required 
+            />
+          </div>
+          <div className='remember-forgot'>
+            <label><input type='checkbox' />Remember me</label>
+            <a href='#'>Forgot Password?</a>
+          </div>
+          <button type='submit'>Login</button>
+          <div className='register-link'>
+            <p>Don't have an account? <a href='#' onClick={toggleActive}>Register</a></p>
+            {errorMessage && <p color = "red"className="error-message">{errorMessage}</p>}
+            
+          </div>
         </form>
       </div>
 
       <div className='form-box register'>
-        <form action=''>
-            <h1>Register</h1>
-            <div className='input-box'>
-                <input type='text' placeholder='Username' required />    
-                <FaUser className='icon' /> 
-            </div>
-            <div className='input-box'>
-                <input type='email' placeholder='Email' required />    
-                <FaEnvelope className='icon' /> 
-            </div>
-            <div className='input-box'>
-            <input type='password' placeholder='Password' required />
-                <FaLock className='icon' />
-            </div>
-            <button type='submit' >Register</button>
-            <div className='register-link'>
-                <p>Already have an account? <a href='#' onClick={toggleActive}>Login</a></p>
-            </div>
+        <form onSubmit={handleRegister}>
+          <h1>Register</h1>
+          <div className='input-box'>
+            <FaUser className='icon' />
+            <input 
+              type='text' 
+              placeholder='Full Name' 
+              value={fullname}
+              onChange={(e) => setFullname(e.target.value)}
+              required 
+            />    
+          </div>
+          <div className='input-box'>
+            <FaEnvelope className='icon' />
+            <input 
+              type='email' 
+              placeholder='Email' 
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required 
+            />    
+          </div>
+          <div className='input-box'>
+            <FaPhone className='icon' />
+            <input 
+              type='text' 
+              placeholder='Phone Number'  
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+              required 
+            />
+          </div>
+          <button type='submit'>Register</button>
+          <div className='register-link'>
+            <p>Already have an account? <a href='#' onClick={toggleActive}>Login</a></p>
+            {errorMessage && <p color='red' className="error-message">{errorMessage}</p>}
+          </div>
         </form>
       </div>
     </div>
   );
 };
 
-export default Login
+export default Login;
